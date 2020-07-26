14116d9f11a0b6be08bca88871bdb8ce157f3de6
Shrishail Gharat <gharatshri014@gmail.com>
2020-07-26 17:38:30 +0530

    Split feature added
    Now when any card is clicked the preview pane opens up on the right.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 #Company enquiry webpage UI
 This webpage is an enquiries UI design build using HTML, CSS, Bootstrap and jQuery.
+##Update : 26/07/2020
+Added the on click split feature.
--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     $("#toggle-sidebar").on("click", function() {
         $("#sidebar").toggleClass("active");
+        $("#card-list").style.width = "100%";
     });
 });
+
+
+function toggleInfoPane() {
+    $("#card-list").addClass("col-lg-6");
+    $("#card-list").removeClass("col-lg-12");
+    $("#card-info").addClass("col-lg-6");
+
+}
+
+function closeInfoPane() {
+    $("#card-list").removeClass("col-lg-6");
+    $("#card-list").addClass("col-lg-12");
+    $("#card-info").removeClass("col-lg-6");
+}
